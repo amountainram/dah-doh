f1ad74ee141c960a130e503dc20ff3598b40f3c6
Umberto Toniolo <amountainram@gmail.com>
2023-03-26 19:20:19 +0200

    es5 test + ci/cd fix

--- e2e/umd.spec.ts
+++ e2e/umd.spec.ts
@@ -1,10 +1,12 @@
 import test, {expect} from '@playwright/test'
 
 const url = 'http://localhost:4173/e2e'
+const umd6 = `${url}/umd`
+const umd5 = `${url}/es5`
 
 test.describe('umd package tests', () => {
-  test('should perform a PTR resolution using umd package', async ({page}) => {
-    await page.goto(url)
+  test('[umd es6+]: should perform a PTR resolution using umd package', async ({page}) => {
+    await page.goto(umd6)
 
     const addresses = await page.evaluate(() =>
       window.dahDoh.promises
@@ -14,8 +16,35 @@ test.describe('umd package tests', () => {
     expect(addresses).toEqual(['dns.google'])
   })
 
-  test('should perform a NS resolution using umd package', async ({page}) => {
-    await page.goto(url)
+  test('[umd es6+]: should perform a NS resolution using umd package', async ({page}) => {
+    await page.goto(umd6)
+
+    const addresses = await page.evaluate(() =>
+      window.dahDoh.promises
+        .resolveNs('google.com')
+    )
+
+    expect(addresses.sort()).toEqual([
+      'ns1.google.com',
+      'ns2.google.com',
+      'ns3.google.com',
+      'ns4.google.com',
+    ])
+  })
+
+  test('[umd es5]: should perform a PTR resolution using umd package', async ({page}) => {
+    await page.goto(umd5)
+
+    const addresses = await page.evaluate(() =>
+      window.dahDoh.promises
+        .resolvePtr('8.8.8.8.in-addr.arpa')
+    )
+
+    expect(addresses).toEqual(['dns.google'])
+  })
+
+  test('[umd es5]: should perform a NS resolution using umd package', async ({page}) => {
+    await page.goto(umd5)
 
     const addresses = await page.evaluate(() =>
       window.dahDoh.promises
